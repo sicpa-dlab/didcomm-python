176dba07b5b09d999671b0f2091ed691018f9e0f
ashcherbakov <alexander.sherbakov@dsr-corporation.com>
2021-09-07 16:27:22 +0300

    more tests against test vectors

--- tests/unit/common.py
+++ tests/unit/common.py
@@ -1,17 +1,35 @@
+import copy
+
 from authlib.common.encoding import json_loads
 
 from didcomm.core.utils import parse_base64url_encoded_json
-from didcomm.unpack import unpack
+from didcomm.unpack import unpack, Metadata
 from tests.test_vectors.test_vectors_common import TestVector, TEST_MESSAGE
 
 
-def decode_and_remove_signatures(jws: str) -> dict:
+def decode_and_remove_jws_signatures(jws: str) -> dict:
     jws = json_loads(jws)
     jws["payload"] = parse_base64url_encoded_json(jws["payload"])
     for s in jws["signatures"]:
         s["protected"] = parse_base64url_encoded_json(s["protected"])
         del s["signature"]
     return jws
+
+
+def decode_jwe_headers(jwe: str) -> dict:
+    jwe = json_loads(jwe)
+    protected = parse_base64url_encoded_json(jwe["protected"])
+    del protected["epk"]
+    recipients = jwe["recipients"]
+    for r in recipients:
+        del r["encrypted_key"]
+    return {"protected": protected, "recipients": recipients}
+
+
+def remove_signed_msg(metadata: Metadata) -> Metadata:
+    metadata = copy.deepcopy(metadata)
+    metadata.signed_message = None
+    return metadata
 
 
 async def unpack_test_vector(test_vector: TestVector, resolvers_config):
--- tests/unit/test_pack_unpack_anon_crypt.py
+++ tests/unit/test_pack_unpack_anon_crypt.py
@@ -1,53 +1,66 @@
 import pytest
 
-from didcomm.common.algorithms import AnonCryptAlg
+from didcomm.common.types import DID_OR_DID_URL
 from didcomm.pack_encrypted import pack_encrypted, PackEncryptedConfig
 from didcomm.unpack import unpack
 from tests.test_vectors.test_vectors_anon_encrypted import (
     TEST_ENCRYPTED_DIDCOMM_MESSAGE_ANON,
 )
-from tests.test_vectors.test_vectors_common import TEST_MESSAGE, BOB_DID
-from tests.unit.common import unpack_test_vector
+from tests.test_vectors.test_vectors_common import TEST_MESSAGE, BOB_DID, TestVector
+from tests.unit.common import unpack_test_vector, decode_jwe_headers
 
 
 @pytest.mark.asyncio
-async def test_unpack_encrypted_anoncrypt_xc20p(resolvers_config_bob):
+async def test_unpack_anoncrypt_xc20p(resolvers_config_bob):
     await unpack_test_vector(
         TEST_ENCRYPTED_DIDCOMM_MESSAGE_ANON[0], resolvers_config_bob
     )
 
 
 @pytest.mark.asyncio
-async def test_unpack_encrypted_anoncrypt_a256cbc(resolvers_config_bob):
+async def test_unpack_anoncrypt_a256cbc(resolvers_config_bob):
     await unpack_test_vector(
         TEST_ENCRYPTED_DIDCOMM_MESSAGE_ANON[1], resolvers_config_bob
     )
 
 
 @pytest.mark.asyncio
-async def test_unpack_encrypted_anoncrypt_a256gcm(resolvers_config_bob):
+async def test_unpack_anoncrypt_a256gcm(resolvers_config_bob):
     await unpack_test_vector(
         TEST_ENCRYPTED_DIDCOMM_MESSAGE_ANON[2], resolvers_config_bob
     )
 
 
 @pytest.mark.asyncio
-async def test_pack_encrypted_anoncrypt_xc20p(
-    resolvers_config_alice, resolvers_config_bob
+async def test_pack_anoncrypt_recipient_as_did(
+        resolvers_config_alice, resolvers_config_bob
 ):
+    await check_pack_anoncrypt(
+        to=BOB_DID,
+        test_vector=TEST_ENCRYPTED_DIDCOMM_MESSAGE_ANON[0],
+        resolvers_config_alice=resolvers_config_alice, resolvers_config_bob=resolvers_config_bob
+    )
+
+
+async def check_pack_anoncrypt(
+        to: DID_OR_DID_URL,
+        test_vector: TestVector,
+        resolvers_config_alice, resolvers_config_bob):
+    expected_metadata = test_vector.metadata
     pack_result = await pack_encrypted(
         resolvers_config_alice,
         TEST_MESSAGE,
-        to=BOB_DID,
+        to=to,
         pack_config=PackEncryptedConfig(
-            enc_alg_anon=AnonCryptAlg.XC20P_ECDH_ES_A256KW, forward=False
+            enc_alg_anon=expected_metadata.enc_alg_anon, forward=False
         ),
     )
-
-    expected_metadata = TEST_ENCRYPTED_DIDCOMM_MESSAGE_ANON[0].metadata
+    pack_result_headers = decode_jwe_headers(pack_result.packed_msg)
+    expected_headers = decode_jwe_headers(test_vector.value)
+    assert expected_headers == pack_result_headers
     assert pack_result.to_kids == expected_metadata.encrypted_to
-    assert pack_result.from_kid is None
-    assert pack_result.sign_from_kid is None
+    assert pack_result.from_kid == expected_metadata.encrypted_from
+    assert pack_result.sign_from_kid == expected_metadata.sign_from
 
     unpack_result = await unpack(resolvers_config_bob, pack_result.packed_msg)
     assert unpack_result.message == TEST_MESSAGE
