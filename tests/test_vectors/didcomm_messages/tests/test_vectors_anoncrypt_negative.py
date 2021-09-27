95bbd6c129fea95ae67c52fcb1dfac08624cfbbd
Nikita Spivachuk <nikita.spivachuk@dsr-corporation.com>
2021-09-27 13:11:07 +0300

    Added test verifying validation of point on curve

--- tests/test_vectors/didcomm_messages/tests/test_vectors_anoncrypt_negative.py
+++ tests/test_vectors/didcomm_messages/tests/test_vectors_anoncrypt_negative.py
@@ -1,3 +1,5 @@
+from authlib.common.encoding import json_dumps
+
 from didcomm.errors import MalformedMessageError
 from tests.test_vectors.common import TTestVectorNegative
 from tests.test_vectors.didcomm_messages.spec.spec_test_vectors_anon_encrypted import (
@@ -17,3 +19,22 @@ INVALID_MESSAGES = [
 INVALID_ANONCRYPT_TEST_VECTORS = [
     TTestVectorNegative(value, MalformedMessageError) for value in INVALID_MESSAGES
 ]
+
+ANONCRYPT_MESSAGE_P256_XC20P_EPK_WRONG_POINT = json_dumps(
+    {
+        "protected": "eyJ0eXAiOiJhcHBsaWNhdGlvbi9kaWRjb21tLWVuY3J5cHRlZCtqc29uIiwiYWxnIjoiRUNESC1FUytBMjU2S1ciLCJlbmMiOiJYQzIwUCIsImFwdSI6bnVsbCwiYXB2Ijoiei1McXB2VlhEYl9zR1luM21qUUxwdXUyQ1FMZXdZdVpvVFdPSVhQSDNGTSIsImVwayI6eyJjcnYiOiJQLTI1NiIsImt0eSI6IkVDIiwieCI6IkZSQW1UQmljUFZJXy1aRnF2WEJwNzZhV2pZM0gzYlpGZlhocHRUNm1ETnciLCJ5IjoiLXZ0LTFIaHRvVjBwN2xrbGIxTnRvMWRhU0lqQnV3cVZzbGIwcC1uOWRrdyJ9fQ==",
+        "recipients": [
+            {
+                "header": {"kid": "did:example:bob#key-p256-1"},
+                "encrypted_key": "scQxV9YQ4mQrUHgl6yAnBFDXNZAiIs_15bmoErUmoYm0HtuRclPoQg",
+            },
+            {
+                "header": {"kid": "did:example:bob#key-p256-2"},
+                "encrypted_key": "CqZ-HDH2j0NC-eoUueNLKyAuMQXjQyw8bJHYM2f-lxJVm3eXCdmm2g",
+            },
+        ],
+        "iv": "Vg1uyuQKrU6Kw8OJK38WCpYFxW0suAP9",
+        "ciphertext": "2nIm3xQcFR3HXbUPF1HS_D92OGVDvL0nIi6O5ol5tnMIa09NxJtbVAYIG7ZrkT9314PqXn_Rq77hgGE6FAOgO7aNYLyUJh0JCC_i2p_XOWuk20BYyBsmmRvVpg0DY3I1Lb-Vg1pT9pEy09gsMSLhbfqk0_TFJB1rcqzR8W0YZB5mX_53nMRf1ZatDEg4rDogSekWEGTBnlTNRua8-zoI4573SfgJ-ONt7Z_KbGO-sdRkmqXhfYNcbUyoMF9JSa-kraVuWHZP9hTz8-7R020EXfb4jodMWVOMMAiJYk1Cd7tetHXpLPdtuokaapofmtL_SNftAX2CB6ULf0axrHUNtvUyjAPvpgvSuvQuMrDlaXn16MQJ_q55",
+        "tag": "etLTQvKsTvF629fykLiUDg",
+    }
+)
--- tests/unit/unpack/test_unpack_encrypted_negative.py
+++ tests/unit/unpack/test_unpack_encrypted_negative.py
@@ -1,9 +1,11 @@
 import pytest
 
+from didcomm.errors import MalformedMessageError
 from didcomm.unpack import unpack
 from tests.test_vectors.common import TTestVectorNegative
 from tests.test_vectors.didcomm_messages.tests.test_vectors_anoncrypt_negative import (
     INVALID_ANONCRYPT_TEST_VECTORS,
+    ANONCRYPT_MESSAGE_P256_XC20P_EPK_WRONG_POINT,
 )
 from tests.test_vectors.didcomm_messages.tests.test_vectors_authcrypt_negative import (
     INVALID_AUTHCRYPT_TEST_VECTORS,
@@ -26,3 +28,9 @@ async def test_unpack_anoncrypt_message(
 ):
     with pytest.raises(test_vector.exc):
         await unpack(resolvers_config_bob, test_vector.value)
+
+
+@pytest.mark.asyncio
+async def test_unpack_anoncrypt_message_epk_wrong_point(resolvers_config_bob):
+    with pytest.raises(MalformedMessageError):
+        await unpack(resolvers_config_bob, ANONCRYPT_MESSAGE_P256_XC20P_EPK_WRONG_POINT)
