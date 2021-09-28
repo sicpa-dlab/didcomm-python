c83ce7fff75fd4f2760ed1371ec7f6a63af3e7ac
ashcherbakov <alexander.sherbakov@dsr-corporation.com>
2021-09-28 11:32:31 +0300

    fix packaging
    - include all didcomm packages (except tests)
    - depend on the fixed commit of authlib (until it's released to pypi)

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 import pathlib
 
-from setuptools import setup
+from setuptools import setup, find_packages
 
 HERE = pathlib.Path(__file__).parent
 README = (HERE / "README.md").read_text()
@@ -21,10 +21,10 @@ setup(
         "Programming Language :: Python :: 3",
         "Programming Language :: Python :: 3.7",
     ],
-    packages=["didcomm"],
+    packages=find_packages(exclude=['tests', 'tests.*']),
     install_requires=[
         # TODO switch to pypi once necessary authlib version is released
-        "Authlib@git+https://github.com/lepture/authlib.git",
+        "Authlib@git+https://github.com/lepture/authlib.git@7bfd5590cc365803633c56e784b43494589abff2",
         "pycryptodomex~=3.10",
         "attrs~=21.2",  # TODO explore lowest acceptable version
         "packaging~=21.0",  # TODO explore lowest acceptable version
