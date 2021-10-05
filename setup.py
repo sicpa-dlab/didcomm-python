c71be46705ccc3991c619908d9f2582d3172ea36
Andrey Kononykhin <andkononykhin@gmail.com>
2021-10-05 12:56:14 +0300

    improves metadata
    Signed-off-by: Andrey Kononykhin <andkononykhin@gmail.com>

--- setup.py
+++ setup.py
@@ -1,27 +1,7 @@
-import pathlib
+from setuptools import setup
 
-from setuptools import setup, find_packages
-
-HERE = pathlib.Path(__file__).parent
-README = (HERE / "README.md").read_text()
-
+# TODO move remaining things
 setup(
-    name="didcomm",
-    version="0.1.0",
-    description="DIDComm for Python",
-    long_description=README,
-    long_description_content_type="text/markdown",
-    url="https://github.com/sicpa-dlab/didcomm-python",
-    author="SICPA",
-    author_email="DLCHOpenSourceContrib@sicpa.com",
-    license="Apache-2.0",
-    python_requires=">=3.7",
-    classifiers=[
-        "License :: OSI Approved :: Apache Software License",
-        "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.7",
-    ],
-    packages=find_packages(exclude=["tests", "tests.*"]),
     install_requires=[
         # TODO switch to pypi once necessary authlib version is released
         "Authlib@git+https://github.com/lepture/authlib.git@7bfd5590cc365803633c56e784b43494589abff2",
@@ -36,8 +16,8 @@ setup(
             "pytest-xdist==2.3.0",
             "flake8==3.9.2",
             "black==21.9b0",
-            "pytest-mock==3.6.1",  # NOTE python 3.6+
-            "mock==4.0.3",  # NOTE python 3.6+
+            "pytest-mock==3.6.1",  # python 3.6+
+            "mock==4.0.3",  # python 3.6+
         ]
-    },
+    }
 )
