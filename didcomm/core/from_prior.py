55455c2b0a7f3b5026b23e972bb09aacd024a018
Sacha Kozma <sacha.kozma@sicpa.com>
2022-12-06 10:35:05 +0100

    Rename is_did_url -> is_did_with_uri_fragment

--- didcomm/core/from_prior.py
+++ didcomm/core/from_prior.py
@@ -7,7 +7,7 @@ from authlib.jose.errors import BadSignatureError
 from didcomm.common.resolvers import ResolversConfig
 from didcomm.common.types import DID_URL
 from didcomm.core.keys.sign_keys_selector import find_signing_key, find_verification_key
-from didcomm.core.utils import extract_key, extract_sign_alg, is_did_url, get_did
+from didcomm.core.utils import extract_key, extract_sign_alg, is_did_with_uri_fragment, get_did
 from didcomm.errors import (
     MalformedMessageError,
     MalformedMessageCode,
@@ -121,9 +121,9 @@ def __extract_from_prior_kid(from_prior_jwt: str) -> DID_URL:
         from_prior_jwt = to_bytes(from_prior_jwt)
         protected_segment = from_prior_jwt.split(b".")[0]
         protected = json_loads(urlsafe_b64decode(protected_segment).decode("utf-8"))
-        if not is_did_url(protected.get("kid")):
+        if not is_did_with_uri_fragment(protected.get("kid")):
             raise DIDCommValueError(
-                f"from_prior `kid` value is not a valid DID URL: {protected.get('kid')}"
+                f"from_prior `kid` value is not a valid DID URL containing a fragment: {protected.get('kid')}"
             )
         return protected["kid"]
     except Exception as exc:
